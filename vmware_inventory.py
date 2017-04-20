e68f1d941b11ce1a321093b09c6f87d4752e5b83
napalm255 <napalm255@gmail.com>
2017-04-20 03:08:51 -0400

    work in progress

--- vmware_inventory.py
+++ vmware_inventory.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""Ansible VMWare Inventory
+"""Ansible VMWare Inventory.
 
 Dynamic inventory for ansible.
 """
@@ -11,8 +11,7 @@ from sys import argv
 import json
 import yaml
 from ansible.module_utils.vmware import connect_to_api, gather_vm_facts
-from ansible.module_utils.vmware import find_vm_by_name, find_cluster_by_name
-from ansible.module_utils.vmware import wait_for_task
+from ansible.module_utils.vmware import find_cluster_by_name
 
 __title__ = 'VMWare Inventory'
 __author__ = 'Brad Gibson'
@@ -23,6 +22,7 @@ __config__ = 'config.yml'
 
 def main():
     """Entry point."""
+    # pylint: disable = too-many-locals
     log_file, log_level = (None, logging.INFO)
     if '--debug' in argv:
         log_level = logging.DEBUG
@@ -30,8 +30,6 @@ def main():
     log_format = '%(levelname)s: %(message)s'
     logging.basicConfig(filename=log_file, level=log_level, format=log_format)
     logging.debug('running %s', __file__)
-
-    the_list = {}
 
     def module():
         """Mock module."""
@@ -42,22 +40,38 @@ def main():
 
     content = connect_to_api(module)
     logging.debug(content)
+    # pylint: disable = no-member
     cluster = find_cluster_by_name(content, module.params['cluster'])
     logging.debug(cluster)
+
+    inv = dict()
+    inv.setdefault('_meta', dict(hostvars=dict()))
+    inv.setdefault('esxi', list())
+    # pylint: disable = too-many-nested-blocks
     for host in cluster.host:
-        logging.debug(host.name)
-        for vm in host.vm:
-            logging.debug(vm.name)
-            facts = dict()
-            facts['uuid'] = vm.summary.config.uuid
-            facts['name'] = vm.summary.config.name
-            facts['guestId'] = vm.summary.config.guestId
-            facts['hostname'] = vm.summary.guest.hostName
-            facts['ipaddress'] = vm.summary.guest.ipAddress
-            facts['customvalues'] = dict()
+        logging.debug('esxi host: %s', host.name)
+        inv['esxi'].append(host.name)
+        for vm_obj in host.vm:
+            vm_name = vm_obj.config.name.lower()
+            vm_ip = vm_obj.guest.ipAddress
+            logging.debug('name: %s, ip: %s', vm_name, vm_ip)
+
+            inv['_meta']['hostvars'].setdefault(vm_name, dict())
+            if module.params['gather_vm_facts']:
+                inv['_meta']['hostvars'][vm_name] = gather_vm_facts(content, vm_obj)
+
+            for prop in module.params['properties']:
+                parts = prop.split('.')
+                vm_prop = getattr(vm_obj, parts[0])
+                parts.pop(0)
+                for part in parts:
+                    vm_prop = getattr(vm_prop, part)
+                inv.setdefault(vm_prop, list())
+                inv[vm_prop].append(vm_name)
+
             cfm = content.customFieldsManager
             # Resolve custom values
-            for value_obj in vm.summary.customValue:
+            for value_obj in vm_obj.summary.customValue:
                 key = value_obj.key
                 if cfm is not None and cfm.field:
                     for field in cfm.field:
@@ -65,12 +79,13 @@ def main():
                             key = field.name
                             # Exit the loop immediately, we found it
                             break
-                facts['customvalues'][key] = value_obj.value
-            logging.debug(facts)
+                group_name = '%s_%s' % (key, value_obj.value)
+                inv.setdefault(group_name, list())
+                inv[group_name].append(vm_name)
 
     if '--list' in argv:
         logging.debug('Display list')
-        print(json.dumps(the_list))
+        print(json.dumps(inv))
     elif '--host' in argv:
         logging.debug('Display host')
         print(json.dumps({}))
