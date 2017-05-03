4e62a5851ca16b044658a2729183feac8e9360c7
napalm255 <napalm255@gmail.com>
2017-05-03 18:56:46 -0400

    changed wording.

--- test.sh
+++ test.sh
@@ -11,7 +11,7 @@ if [[ $_ != $0 ]]; then
   export VMWARE_USERNAME=$(read -p "Username: " && echo $REPLY)
   export VMWARE_PASSWORD=$(read -p "Password: " -s && echo $REPLY)
   echo
-  export VMWARE_CLUSTERS=$(read -p "Cluster Name: " && echo $REPLY)
+  export VMWARE_CLUSTERS=$(read -p "Cluster Name(s): " && echo $REPLY)
   export VMWARE_VALIDATE_CERTS=false
 else
   echo "usage: source $_"
