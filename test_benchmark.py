d0f701af74759e5ede18f57f319c4a5c179489fc
napalm255 <napalm255@gmail.com>
2017-05-10 16:03:25 -0400

    clean up

--- test_benchmark.py
+++ test_benchmark.py
@@ -13,19 +13,18 @@ except ImportError:
     exit(1)
 
 
-def test_ansible_list_hosts(benchmark):
-    """Test performance using ansible-playbook --list-hosts."""
+def test_inventory(benchmark):
+    """Test inventory."""
 
     @benchmark
-    def run():
-        """Run test."""
+    def test_ansible_list_hosts():
+        """Test performance using ansible-playbook --list-hosts."""
+        # pylint: disable=unused-variable
         dir_path = os.path.dirname(os.path.realpath(__file__))
         play = '%s/test.yml' % dir_path
         inventory = '%s/vmware_inventory.py' % dir_path
         res = subprocess.call(['ansible-playbook', play, '-i', inventory, '--list-hosts'])
         return False if res > 0 else True
-
-    assert run()
 
 
 if __name__ == '__main__':
